a38df5401b829e7bf7da3d1dff4d7c6fc2bc70d4
Ertunç Efeoğlu <ertuncefeoglu@gmail.com>
2019-08-28 23:01:57 +0300

    delete multiple functionality added

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,13 @@
+class HomeController < ApplicationController
+
+  def trash_em
+      @trash_em = List.where(:completed => true).destroy_all  
+      redirect_to lists_url, notice: 'All marked items were succesfully deleted.'
+  end
+
+  def trash_em_all
+      @trash_em_all = List.all.destroy_all  
+      redirect_to lists_url, notice: 'All items were succesfully deleted.'
+  end  
+
+end
