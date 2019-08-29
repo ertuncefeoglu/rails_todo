577a3b95aa1eefba79e136bed583c33c04ba8c63
Ertunç Efeoğlu <ertuncefeoglu@gmail.com>
2019-08-29 14:39:46 +0300

    change list route redirect to root

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,12 +2,12 @@ class HomeController < ApplicationController
 
   def trash_em
       @trash_em = List.where(:completed => true).destroy_all  
-      redirect_to lists_url, notice: 'All marked items were succesfully deleted.'
+      redirect_to root_url, notice: 'All marked items were succesfully deleted.'
   end
 
   def trash_em_all
       @trash_em_all = List.all.destroy_all  
-      redirect_to lists_url, notice: 'All items were succesfully deleted.'
+      redirect_to root_url, notice: 'All items were succesfully deleted.'
   end  
 
 end
